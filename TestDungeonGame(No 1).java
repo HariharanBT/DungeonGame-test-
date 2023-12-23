57fe57fd8099f1aa7d716b30d1efd14e49d583b7
Hari Haran <148673523+HariharanBT@users.noreply.github.com>
2023-12-23 15:25:13 +0530

    Update TestDungeonGame(No 1).java
    Update the Question 3

--- TestDungeonGame(No 1).java
+++ TestDungeonGame(No 1).java
@@ -23,21 +23,37 @@ public class DungeonGame {
 		int goldRow = scanner.nextInt();
 		int goldColumn = scanner.nextInt();
 
-		String result = calculateResult(rows, columns, adventurerRow, adventurerColumn, monsterRow, monsterColumn,
-				goldRow, goldColumn);
-
-		System.out.println(result);
+		calculateResult(rows, columns, adventurerRow, adventurerColumn, monsterRow, monsterColumn, goldRow, goldColumn);
 	}
 
-	private static String calculateResult(int rows, int columns, int adventurerRow, int adventurerColumn,
-			int monsterRow, int monsterColumn, int goldRow, int goldColumn) {
+	private static void calculateResult(int rows, int columns, int adventurerRow, int adventurerColumn, int monsterRow,
+			int monsterColumn, int goldRow, int goldColumn) {
 		int adventurerSteps = Math.abs(adventurerRow - goldRow) + Math.abs(adventurerColumn - goldColumn);
 		int monsterSteps = Math.abs(monsterRow - goldRow) + Math.abs(monsterColumn - goldColumn);
 
 		if (adventurerSteps <= monsterSteps) {
-			return "Minimum number of steps: " + adventurerSteps;
+			System.out.println("Minimum number of steps: " + adventurerSteps);
+			printPath(adventurerRow, adventurerColumn, goldRow, goldColumn);
 		} else {
-			return "No possible solution";
+			System.out.println("No possible solution");
 		}
+	}
+
+	private static void printPath(int adventurerRow, int adventurerColumn, int goldRow, int goldColumn) {
+		System.out.print("Path: (" + adventurerRow + "," + adventurerColumn + ")");
+		while (adventurerRow != goldRow || adventurerColumn != goldColumn) {
+			if (adventurerRow < goldRow) {
+				adventurerRow++;
+			} else if (adventurerRow > goldRow) {
+				adventurerRow--;
+			}
+			if (adventurerColumn < goldColumn) {
+				adventurerColumn++;
+			} else if (adventurerColumn > goldColumn) {
+				adventurerColumn--;
+			}
+			System.out.print(" -> (" + adventurerRow + "," + adventurerColumn + ")");
+		}
+		System.out.println();
 	}
 }
